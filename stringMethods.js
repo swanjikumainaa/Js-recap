3b998a4cf23407ba9181cbc02caf7cdb18bddfd7
student <swanjikumaina@gmail.com>
2023-05-22 17:17:45 +0300

    mathObject and string methods

---
+++ stringMethods.js
@@ -0,0 +1,21 @@
+// length: The string length method returns the
+//  number of characters in a string included empty space.
+let js = 'JavaScript'
+console.log(js.length)         // 10
+let firstName = 'Asabeneh'
+console.log(firstName.length)  // 8
+
+// Accessing characters in a string: We can access each 
+// character in a string using its index. In programming, 
+// counting starts from 0. The first index of the string is zero,
+//  and the last index is the length of the string minus one.
+
+let myName  ="Susan Maina";
+
+console.log(myName[0]);
+
+// toUpperCase(): this method changes the string to uppercase letters.
+console.log(myName.toUpperCase());
+
+// toLowerCase(): this method changes the string to lowercase letters
+console.log(myName.toLowerCase());
